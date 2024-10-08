12e5b157ceef846903fd1fb43b794fc4dfb203d9
tccmw <jungmwoo6248@gmail.com>
2024-10-08 20:20:52 +0900

    App.js

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './LoginPage.jsx'
+import './index.css'
+import { Route,  BrowserRouter, Routes} from 'react-router-dom'
+import Join from './JoinPage.jsx'
+import Main from './mainPage.jsx'
+
+createRoot(document.getElementById('root')).render( 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main/>}></Route>
+        <Route path='/Login' element={<Login/>}></Route>
+        <Route path='/Join' element={<Join/>}></Route>
+      </Routes>
+    </BrowserRouter>
+)
